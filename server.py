b2bd9d7efa1a9b4bd652bfbd21298fdfb5915e59
imbau <bautidemariogimenez@gmail.com>
2026-01-03 19:16:09 -0300

    Update server to use environment variable for port

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from call_ai import call_openrouter
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,6 @@ def responder():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
