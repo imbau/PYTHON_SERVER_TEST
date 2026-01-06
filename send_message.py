992a9c567d18b3caa67885050b7bcc9b167cb91e
imbau <bautidemariogimenez@gmail.com>
2026-01-06 17:26:03 -0300

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,12 +1,9 @@
 from call_ai import call_openrouter
 import os
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
-
-WSP_TOKEN = "EAAU7yAI6f0ABQRCx1RXlUiSETR26jvaY6VCIfywL561lMpH3rZAUPBfqXYJY2Y79HkgR6BNZCreGFZAQc1UX1PKnZAyKrHmnjlPHpmvCxSGfdSdX6cICRyvS1KZCLL8UNRBTRhnI5HBbs5DUYapFJ7ecwby7omCoZBKNvoUQ61r07HSYF1wMwUBsHYOydxarwDndkZBbv5gCwIUZC1zNOW03jFCb7H6udwFOTMSEcndZA"
-PHONE_NUMBER_ID = "904783266046247"
+WSP_TOKEN = os.getenv('WSP_TOKEN')
+PHONE_NUMBER_ID = os.getenv('PHONE_NUMBER_ID')
 
 def send_message(to, user_text):
     text = call_openrouter(user_text)
